b3a0e731df8fa2b1994d83d662cbcb1465e37405
occiey <oki.sashiki@gmail.com>
2022-07-23 13:40:16 +0900

    modified:ヘッダーコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <CommonHeader />
   <router-view />
 </template>
+<script>
+// @ is an alias to /src
+import CommonHeader from "@/components/Header.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    CommonHeader,
+  },
+};
+</script>
 
 <style>
 #app {
@@ -13,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
