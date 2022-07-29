199223983b97d30d375a3bd499ef4998381323c7
occiey <oki.sashiki@gmail.com>
2022-07-29 17:51:07 +0900

    add:header部分コーディング

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,16 @@
 <template>
-  <div id="app">
-    <h2>積読さん</h2>
-    <el-menu mode="horizontal" background-color="#545c64" text-color="#fff">
-      <el-menu-item index="1">
-        <router-link to="/">積んだ本</router-link>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <router-link to="/search">本を探す</router-link>
-      </el-menu-item>
-      <!-- <el-submenu index="3">
-        <template v-slot:title>SUPPORT</template>
-        <el-menu-item index="3-1">
-          <a href="https://wings.msn.to/" target="help">Author</a>
-        </el-menu-item>
-        <el-menu-item index="3-2">
-          <a href="https://www.sbcr.jp" target="help">Publisher</a>
-        </el-menu-item>
-      </el-submenu> -->
-    </el-menu>
-  </div>
-    <router-view></router-view>
+  <header class="pt-16 px-8">
+    <h1 class="flex justify-center w-32 mx-auto">
+      <img v-bind:src="require('./assets/images/logo.png')" alt="積読さん">
+    </h1>
+    <nav>
+      <ul class="flex justify-center pt-8 px-8">
+        <li><router-link class="btn _btn-gray rounded-full p-2 px-6 mr-2" to="/">積んだ本</router-link></li>
+        <li><router-link class="btn _btn-gray rounded-full p-2 px-6" to="/search">本を探す</router-link></li>
+      </ul>
+    </nav>
+    <p class="flex justify-center pt-8 px-8 text-center">積読さんはただ気になる本を積むだけのメモサイトです。</p>
+  </header>
+
+  <router-view></router-view>
 </template>
-
-<script>
-  export default {
-    name: 'App',
-  }
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  width: 800px;
-  margin: 0 auto;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-#nav {
-  margin-bottom: 15px;
-}
-</style>
